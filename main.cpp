8ec4d0428932dbefa37c9c70d5ccdeb46c054e08
Thomas Nesset <thomasne@uia.no>
2022-04-24 13:05:00 +0200

    createAlarm function added

--- main.cpp
+++ main.cpp
@@ -8,13 +8,16 @@ using namespace std;
 
 void startClock();
 bool checkAlarm();
+string createAlarm();
 
 int main ()
 {
     setenv("TZ", "UTC-2", 1);
     tzset();
 
-    startClock();
+    // startClock();
+
+    string alarm = createAlarm();
 
     return 0;
 }
@@ -54,4 +57,41 @@ bool checkAlarm() {
         cout << "Ring ring ring" << endl;
         return true;
     }
+}
+
+string createAlarm() {
+    char buffer[50];
+
+    int h = 0;
+    int m = 0;
+    int i = 0;
+
+    cout << "00:00" << endl;
+
+    int c;
+    while (true) {
+        cin >> c;
+
+        if (i == 0 && c == 1) {
+            if (h == 23)
+                h = -1;
+            h++;
+        } else if (i == 0 && c == 0) {
+            i++;
+        } else if (i == 1 && c == 1) {
+            if (m == 59)
+                m = -1;
+            m++;
+        } else if (i == 1 && c == 0) {
+            i++;
+        }
+
+        if (i == 2) {
+            break;
+        }
+        sprintf(buffer, "%.2d:%.2d", h, m);
+        cout << buffer << endl;
+    }
+
+    return buffer;
 }
